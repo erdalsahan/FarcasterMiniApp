1ad7b639bb8f024a10ced25c36be6bbe321c2c51
Erdal ŞAHAN <dhm23198@dhmi.root>
2025-10-22 12:00:42 +0300

    Initial commit - Airdrop Hunter Mini App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { sdk } from "@farcaster/miniapp-sdk";
 import StartGame from './components/StartGame'
 import Shooting from './components/Shooting'
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false)
+    useEffect(() => {
+    const init = async () => {
+      try {
+        await sdk.actions.ready(); // ✅ Farcaster'a "hazırım" sinyali gönderir
+        console.log("Mini app ready ✅");
+      } catch (err) {
+        console.error("SDK ready hatası:", err);
+      }
+    };
 
+    init();
+  }, []);
   return (
     <>
       {!isGameStarted && <StartGame  setIsGameStarted={setIsGameStarted}/>}
